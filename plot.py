428193517d742b6bc6c69bb767af60dc471f0971
Ali-Najar <anajar13750@gmail.com>
2025-07-17 16:54:27 +0200

    plot added

---
+++ plot.py
@@ -0,0 +1,91 @@
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+CACHE_FILE = 'cache/efficient_error_data.npz'
+BANDED_CACHE_FILE = 'cache/efficient_banded_cache.npz'
+
+prev_len = 0
+prev_len_banded = 0
+
+errors_for_A_BandMF = []
+# D A1_sqrt / A1_sqrt
+errors_for_D_A1_sqrt = []
+# A D_toep^{-1/2} / D_toep^{1/2}
+errors_for_A_D_sqrt_inv = []
+# A D_toep^{-1} / D_toep
+errors_for_A_D_inv = []
+# A I / I
+errors_for_A_I = []
+
+if os.path.exists(CACHE_FILE):
+    # load whatever was computed before
+    data = np.load(CACHE_FILE)
+    errors_for_D_A1_sqrt    = list(data['errors_for_D_A1_sqrt'])
+    errors_for_A_I          = list(data['errors_for_A_I'])
+    errors_for_A_D_sqrt_inv = list(data['errors_for_A_D_sqrt_inv'])
+    errors_for_A_D_inv      = list(data['errors_for_A_D_inv'])
+    prev_len = len(errors_for_A_I)
+
+if os.path.exists(BANDED_CACHE_FILE):
+    # load whatever was computed before
+    data = np.load(BANDED_CACHE_FILE)
+    errors_for_A_BandMF     = list(data.get('errors_for_A_BandMF', []))
+    prev_len_banded = len(errors_for_A_BandMF)
+
+exponents = np.arange(0, prev_len + 1)   # 2^0 ... 2^12 = 4096
+n_range   = 2**exponents
+
+plot_ratio = True
+
+plt.figure()
+
+plt.rcParams.update({
+    # use LaTeX to render all text
+    "text.usetex":    True,
+    # default font family for text
+    "font.family":    "serif",
+    # ask LaTeX for Computer Modern Roman
+    "font.serif":     ["Computer Modern Roman"],
+    # match your desired size/weight
+    "font.size":      16,
+    "font.weight":    "normal",
+    # ensure math uses the same font
+    "text.latex.preamble": r"\usepackage{amsmath}"
+})
+
+if plot_ratio == False:
+    plt.plot(n_range, errors_for_A_I, marker='^', label='(i) $\mathbf{D} \mathbf{A}_1^{1/2}, \mathbf{A}_1^{1/2}$')
+    plt.plot(n_range, errors_for_A_I, marker='^', label='(ii) $\mathbf{A}, \mathbf{I}$')
+    plt.plot(n_range, errors_for_A_D_sqrt_inv, marker='s', label='(vi) $\mathbf{A} \mathbf{D}_{\mathrm{Toep}}^{-1/2}, \mathbf{D}_{\mathrm{Toep}}^{1/2}$')
+    plt.plot(n_range, errors_for_A_D_inv, marker='D', label='RMSE of (viii) $\mathbf{A} \mathbf{D}_{\mathrm{Toep}}^{-1}, \mathbf{D}_{\mathrm{Toep}}$')
+    plt.plot(n_range, errors_for_A_BandMF, label='Optimal')
+    plt.xlabel('Matrix Size')
+    plt.xscale('log')
+    plt.ylabel('$\mathrm{RMSE}(\mathbf{B},\mathbf{C})$')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("plots/efficient_error_vs_log_mat_size.pdf", format="pdf")
+    plt.show()
+else:
+    ratios_D_A1_sqrt = [errors_for_D_A1_sqrt[i] / errors_for_A_BandMF[i] for i in range(len(errors_for_D_A1_sqrt))]
+    ratios_AI = [errors_for_A_I[i] / errors_for_A_BandMF[i] for i in range(len(errors_for_A_I))]
+    ratios_D_toep = [errors_for_A_D_inv[i] / errors_for_A_BandMF[i] for i in range(len(errors_for_A_D_inv))]
+    ratios_D_toep_sqrt = [errors_for_A_D_sqrt_inv[i] / errors_for_A_BandMF[i] for i in range(len(errors_for_A_D_sqrt_inv))]
+    plt.plot(n_range,ratios_AI, marker='^', label='(i) $\mathbf{D} \mathbf{A}_1^{1/2}, \mathbf{A}_1^{1/2}$')
+    plt.plot(n_range,ratios_AI, marker='^', label='(ii) $\mathbf{A}, \mathbf{I}$')
+    plt.plot(n_range,ratios_D_toep_sqrt, marker='s' , label='(vi) $\mathbf{A} \mathbf{D}_{\mathrm{Toep}}^{-1/2}, \mathbf{D}_{\mathrm{Toep}}^{1/2}$')
+    plt.plot(n_range,ratios_D_toep, marker='D', label='(viii) $\mathbf{A} \mathbf{D}_{\mathrm{Toep}}^{-1}, \mathbf{D}_{\mathrm{Toep}}$')
+    plt.axhline(
+    y=1.0,
+    linestyle='--',
+    label='Optimal'
+    )
+    plt.xlabel('Matrix Size')
+    plt.xscale('log')
+    plt.ylabel('RMSE Ratio to The Optimal Value')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("plots/efficient_error_ratio_vs_log_mat_size.pdf", format="pdf")
+    plt.show()
